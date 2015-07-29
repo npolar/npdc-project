3e440a4b7f958c5ac3bfcd9f51b87b0976dad58b
srldl <siri.uldal@gmail.com>
2015-07-29 15:41:54 +0200

    form entries added

--- node_modules/npdc-gulp/tasks/lint.js
+++ node_modules/npdc-gulp/tasks/lint.js
@@ -4,13 +4,29 @@ var task = function(gulp, config) {
   var jshint = require('gulp-jshint');
   var fs = require('fs');
   var path = require('path');
+  var notify = require('gulp-notify');
+  notify.logLevel(0);
 
   var jshintrc = JSON.parse(fs.readFileSync(path.join(__dirname, '../.jshintrc')));
 
   gulp.task('lint', function() {
     return gulp.src(config.src.js)
       .pipe(jshint(jshintrc))
-      .pipe(jshint.reporter('jshint-stylish'));
+      .pipe(jshint.reporter('jshint-stylish'))
+      .pipe(notify(function (file) {
+        if (file.jshint.success) {
+          return false;
+        }
+
+        var errors = file.jshint.results.map(function (data) {
+          if (data.error) {
+            return "(" + data.error.line + ':' + data.error.character + ') ' + data.error.reason;
+          }
+        }).join("\n");
+        return {
+          message: file.relative + " (" + file.jshint.results.length + " errors)\n" + errors,
+          title: 'Gulp jshint'};
+      }));
       //.pipe(jshint.reporter('fail'));
   });
 };
