a7c7c2f0c87693b04e2777147b1e22b6c1fb8d80
cnrdh <conrad@npolar.no>
2018-12-17 11:03:45 +0100

    Fixed merge issues

--- src/show/ProjectShowController.js
+++ src/show/ProjectShowController.js
@@ -1,11 +1,6 @@
 'use strict';
-
-<<<<<<< Updated upstream
-function ProjectShowController($scope, $controller, $q,
-   NpdcWarningsService, Project, ProjectModel, ProjectWarningsService, Dataset, Publication, npdcAppConfig) {
-=======
-function ProjectShowController($scope, $controller, $q, NpdcWarningsService, Project, ProjectModel, Dataset, Publication, npdcAppConfig) {
->>>>>>> Stashed changes
+function ProjectShowController($scope, $controller, $q, NpdcWarningsService,
+   Project, ProjectModel, ProjectWarningsService, npdcAppConfig) {
   'ngInject';
 
   $controller('NpolarBaseController', {$scope: $scope});
@@ -20,10 +15,6 @@ function ProjectShowController($scope, $controller, $q, NpdcWarningsService, Pro
     return false;
   };
 
-
-<<<<<<< Updated upstream
-
-
   this.showAction = () => {
     $scope.show().$promise.then(project => {
       NpdcWarningsService.warnings[project.id] = ProjectWarningsService.warnings(project);
@@ -31,23 +22,6 @@ function ProjectShowController($scope, $controller, $q, NpdcWarningsService, Pro
   };
 
   this.showAction();
-
-=======
-  $scope.show();
->>>>>>> Stashed changes
-
-
-  this.showAction = () => {
-    $scope.show().$promise.then(project => {
-
-        NpdcWarningsService.warnings[project.id] = ProjectModel.warnings(project);
-        //NpdcWarningsService.notices[project.id] = DatasetModel.notices(dataset);
-
-    });
-  };
-
-  this.showAction();
-
 
 }
 
